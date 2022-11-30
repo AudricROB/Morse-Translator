5048a8f2378f964323e23782ec4587161fe13d0f
AudricROB <114215795+AudricROB@users.noreply.github.com>
2022-11-30 11:48:39 +0100

    Create LettreMorse.h

---
+++ LettreMorse.h
@@ -0,0 +1,60 @@
+#include <Arduino.h>
+#ifndef LettreMorse_h
+#define LettreMorse_h
+
+class LettreMorse
+{
+  private:
+    char lettre;
+    void lettreA();
+    void lettreB();
+    void lettreC();
+    void lettreD();
+    void lettreE();
+    void lettreF();
+    void lettreG();
+    void lettreH();
+    void lettreI();
+    void lettreJ();
+    void lettreK();
+    void lettreL();
+    void lettreM();
+    void lettreN();
+    void lettreO();
+    void lettreP();
+    void lettreQ();
+    void lettreR();
+    void lettreS();
+    void lettreT();
+    void lettreU();
+    void lettreV();
+    void lettreW();
+    void lettreX();
+    void lettreY();
+    void lettreZ();
+    void chiffre1();
+    void chiffre2();
+    void chiffre3();
+    void chiffre4();
+    void chiffre5();
+    void chiffre6();
+    void chiffre7();
+    void chiffre8();
+    void chiffre9();
+    void chiffre0();
+    void espace();
+    void grand_espace();
+    void point();
+    void tiret();
+    int _pin;
+    unsigned long currentTime;
+    
+
+  public:
+    LettreMorse();
+    LettreMorse(char lettre);
+    LettreMorse(int pin);
+    void clignoteLED();
+};
+
+#endif
